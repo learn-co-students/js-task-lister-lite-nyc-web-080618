67b777bd677dbfa7045750c0303a6ebecb84d530
bwadlow <bwadlow@gmail.com>
2018-09-20 16:01:09 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,14 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  const taskForm = document.getElementById('create-task-form')
+  const taskContainer = document.getElementById('tasks')
+  taskForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+    const userInput = event.target.querySelector('#new-task-description').value
+    const pTag = document.createElement('p')
+    pTag.innerText = userInput
+
+    taskContainer.appendChild(pTag)
+    event.target.reset()
+  })
+
 });
