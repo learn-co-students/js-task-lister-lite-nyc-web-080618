1fe5e2bb9c2b519ba7aa7f67f7b850eba9afad96
github username <github email address>
2018-09-20 16:25:30 -0400

    Done.

--- src/task.js
+++ src/task.js
@@ -1,2 +1,17 @@
+let taskId = 0;
+let tasks = []
 class Task {
+  constructor(description,listObj) {
+    this.description = description;
+    this.list = listObj
+    this.id = ++taskId;
+    
+    tasks.push(this)
+  }
+
+  delete(list) {
+    let deletion = list.indexOf(this);
+    list.splice(deletion,1);
+    return list;
+  }
 }
--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,11 @@
+let listId = 0
 class TaskList {
+  constructor() {
+    this.id = ++listId
+  }
+  tasks () {
+    return tasks.filter( function (task) {
+      return task.list.id == this.id
+    }.bind(this) )
+  }
 }
