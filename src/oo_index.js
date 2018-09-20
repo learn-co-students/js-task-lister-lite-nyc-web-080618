98c868e0e5d64b0c1e6ffe5ff9feacbbdbb489e5
mccoleman75225 <michaelcoleman19@gmail.com>
2018-09-20 15:48:44 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,31 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  //const taskList = new TaskList();
+
+  const taskForm = document.getElementById('create-task-form')
+  const taskContainer = document.getElementById('tasks')
+  taskForm.addEventListener('submit', function(event){
+    event.preventDefault()
+    const userInput = event.target.querySelector('#new-task-description').value
+    const pTag = document.createElement('p')
+    const deleteButton = document.createElement('BUTTON')
+    deleteButton.setAttribute('value','delete this task')
+    //const newText = userInput + deleteButton
+    //debugger
+    //pTag.innerText = userInput
+
+
+
+
+    function deleteTask(){
+      debugger
+    }
+
+    taskContainer.innerHTML += `<p>${userInput}<button type="button" onclick=deleteTask();>Delete this task!</button></p>`
+    event.target.reset()
+
+
+
+
+  })
 });
