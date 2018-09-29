4ad8c10d3b0052dd1cbe9627219fcd1a09c532cc
github username <github email address>
2018-09-28 21:54:07 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,32 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+
+  const newTaskForm = document.getElementById("create-task-form");
+  const newDescription = document.getElementById("new-task-description");
+  const list = document.getElementById("tasks");
+
+//step 1 add task to the list;
+  newTaskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let newTask = new Task(newDescription.value);
+    list.innerHTML += `<li data-id="${newTask.id}">${newTask.content} <button data-id="${newTask.id}">x</button></li>`;
+    event.target.reset();
+  })
+
+//step 2 delete tasks
+  list.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON"){
+      let taskId = event.target.dataset.id;
+      let targetTask = allTasks.find(task => task.id == taskId);
+      let targetTasks = allTasks.filter(task => task.content === targetTask.content);
+      if (targetTasks.length > 1){
+        for (const task of targetTasks){
+          document.querySelectorAll(`[data-id="${task.id}"]`)[0].remove();
+        }
+      } else {
+        document.querySelector(`[data-id="${targetTasks[0].id}"]`).remove();
+      }
+    }
+  })
+
 });
--- src/task.js
+++ src/task.js
@@ -1,2 +1,11 @@
+let taskId = 0;
+
 class Task {
+  constructor(content){
+    this.id = ++taskId;
+    this.content = content;
+    allTasks.push(this)
+  }
 }
+
+allTasks = [];
